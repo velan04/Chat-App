8a00775f00a8c4c1a64f6f2fc99f0b812c921c6d
velan <velanv611@gmail.com>
2024-12-12 16:50:36 +0530

    live server url added in FE

--- chat-app/src/pages/chat.js/chat.jsx
+++ chat-app/src/pages/chat.js/chat.jsx
@@ -4,7 +4,7 @@ import io from 'socket.io-client';
 let socket;
 
 const Chat = () => {
-  const backendUrl = 'http://localhost:8000';
+  const backendUrl = 'https://chat-app-1hmo.onrender.com/';
   const [user, setUser] = useState('');
   const [room, setRoom] = useState('');
   const [text, setText] = useState('');
